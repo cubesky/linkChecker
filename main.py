f66809c1140c756291195803ff4fe56894589466
cubesky <wqr3011@gmail.com>
2017-03-19 00:38:13 +0800

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+soup = BeautifulSoup(requests.get('https://liyin.date/links').text,'html5lib')
+links=soup.select('li.md-links-item')
+list={}
+for link in links:
+    url=link.find_all('a')[0].get('href')
+    try:
+        response_link=requests.get(url,timeout=20)
+        code=response_link.status_code
+        timeelapsed=response_link.elapsed.total_seconds()
+        print(url +' is ' + str(code) + ' in ' + str(timeelapsed) + 's')
+        list[url]=str(code)
+    except requests.exceptions.Timeout:
+        print(url + ' is Timeout')
+        list[url]='timeout'
+statejson='state('+json.dumps(list)+')'
+os.mkdir('public')
+with open('public/data.jsonp', 'w') as outfile:
+    outfile.write(statejson)
