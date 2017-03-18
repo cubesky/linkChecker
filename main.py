b20f3949c1e92f5784dede32a9fae84efc22f0ac
cubesky <wqr3011@gmail.com>
2017-03-19 01:21:02 +0800

    test for timeout

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ list={}
 for link in links:
     url=link.find_all('a')[0].get('href') 
     try:
-        response_link=requests.get(url,timeout=30)
+        response_link=requests.get(url,timeout=1)
         code=response_link.status_code
         timeelapsed=response_link.elapsed.total_seconds()
         print(url +' is ' + str(code) + ' in ' + str(timeelapsed) + 's')
@@ -16,6 +16,9 @@ for link in links:
     except requests.exceptions.Timeout:
         print(url + ' is Timeout')
         list[url]='timeout'
+    except requests.exceptions.SSLError:
+        print(url + ' is SSLError')
+        list[url]='sslerror'
 statejson='state('+json.dumps(list)+')'
 os.mkdir('public')
 with open('public/data.jsonp', 'w') as outfile:
